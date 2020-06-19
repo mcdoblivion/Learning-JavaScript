617c11972927180ac6e4cc04513938abe27b3993
mcd_oblivion <mcd.oblivion@gmail.com>
2020-06-19 18:12:53 +0700

    Global vs Local variables

--- test.js
+++ test.js
@@ -1,13 +1,12 @@
-function myLocalScope() {
-    'use strict';
-  
-    // Only change code below this line
-    var myVar = "a";
-    console.log('inside myLocalScope', myVar);
-  }
-  myLocalScope();
-  
-  // Run and check the console
-  // myVar is not defined outside of myLocalScope
-  console.log('outside myLocalScope', myVar);
-  
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  var outerWear = "sweater";
+
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
