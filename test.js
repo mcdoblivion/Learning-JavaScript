137e286e471e46f45b71f812a21848529b24d781
mcd_oblivion <mcd.oblivion@gmail.com>
2020-06-19 17:59:47 +0700

    Global scope

--- test.js
+++ test.js
@@ -1,4 +1,23 @@
-function functionWithArgs(p1, p2) {
-    console.log(p1 + p2);
-}
-functionWithArgs(5, 3);
+// Declare the myGlobal variable below this line
+var myGlobal = 10;
+
+function fun1() {
+    // Assign 5 to oopsGlobal Here
+    oopsGlobal = 5;
+  }
+  
+  // Only change code above this line
+  
+  function fun2() {
+    var output = "";
+    if (typeof myGlobal != "undefined") {
+      output += "myGlobal: " + myGlobal;
+    }
+    if (typeof oopsGlobal != "undefined") {
+      output += " oopsGlobal: " + oopsGlobal;
+    }
+    console.log(output);
+  }
+  
+  fun1();
+  fun2();
