ca3578c5620b43dbd3f588bcd4fcb3612c2eb81f
mcd_oblivion <mcd.oblivion@gmail.com>
2020-06-19 18:44:44 +0700

    Comparison with the Strict Equality Operator

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 // Setup
-function testEqual(val) {
-    if (val == 12) {
+function testStrict(val) {
+    if (val === 7) {
         // Change this line
         return "Equal";
     }
     return "Not Equal";
 }
 
-testEqual(10);
+testStrict(10);
