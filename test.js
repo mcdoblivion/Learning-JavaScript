8154af681442173d8bfeead8806286ef7b33ee8f
mcd_oblivion <mcd.oblivion@gmail.com>
2020-06-19 18:36:50 +0700

    Understanding Boolean Values

--- test.js
+++ test.js
@@ -1,15 +1,7 @@
-function nextInLine(arr, item) {
+function welcomeToBooleans() {
     // Only change code below this line
-    arr.push(item);
-    item = arr.shift();
-    return item;
+
+    return true; // Change this line
+
     // Only change code above this line
 }
-
-// Setup
-var testArr = [1, 2, 3, 4, 5];
-
-// Display code
-console.log("Before: " + JSON.stringify(testArr));
-console.log(nextInLine(testArr, 6));
-console.log("After: " + JSON.stringify(testArr));
