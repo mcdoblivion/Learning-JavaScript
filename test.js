ec25793739196c042ad46f78ecd2a3cce83cd0d9
mcd_oblivion <mcd.oblivion@gmail.com>
2020-06-19 18:34:25 +0700

    Stand in Line

--- test.js
+++ test.js
@@ -1,9 +1,15 @@
-// Setup
-var processed = 0;
-
-function processArg(num) {
-    return (num + 3) / 5;
+function nextInLine(arr, item) {
+    // Only change code below this line
+    arr.push(item);
+    item = arr.shift();
+    return item;
+    // Only change code above this line
 }
 
-// Only change code below this line
-processed = processArg(7);
+// Setup
+var testArr = [1, 2, 3, 4, 5];
+
+// Display code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
