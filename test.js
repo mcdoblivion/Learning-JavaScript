f91fc2938a5f1ee0e591e3b74369551166e807d3
mcd_oblivion <mcd.oblivion@gmail.com>
2020-06-19 18:25:49 +0700

    Undefined Value returned from a Function

--- test.js
+++ test.js
@@ -1,3 +1,16 @@
-function timesFive(p) {
-    return p * 5;
+// Setup
+var sum = 0;
+
+function addThree() {
+    sum = sum + 3;
 }
+
+// Only change code below this line
+function addFive() {
+    sum += 5;
+}
+
+// Only change code above this line
+
+addThree();
+addFive();
