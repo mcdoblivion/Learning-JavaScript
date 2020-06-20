9dc4295711b0c863265db7d9c0566ed0b9496e37
mcd_oblivion <mcd.oblivion@gmail.com>
2020-06-20 16:35:18 +0700

    Compare Scopes of the var and let Keywords

--- ES6Challenges.js
+++ ES6Challenges.js
@@ -1,9 +1,10 @@
-let catName;
-let quote;
-function catTalk() {
+function checkScope() {
     "use strict";
-
-    catName = "Oliver";
-    quote = catName + " says Meow!";
+    let i = "function scope";
+    if (true) {
+        let i = "block scope";
+        console.log("Block scope i is: ", i);
+    }
+    console.log("Function scope i is: ", i);
+    return i;
 }
-catTalk();
