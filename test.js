8e183930c58529ebced622d79804c67e5c4fa804
mcd_oblivion <mcd.oblivion@gmail.com>
2020-06-20 15:11:01 +0700

    Replace Loops using Recursion

--- test.js
+++ test.js
@@ -1,8 +1,7 @@
-// Setup
-var myArray = [];
-var i = 10;
-
-// Only change code below this line
-do {
-    myArray.push(i++);
-} while (i < 11);
+function sum(arr, n) {
+    // Only change code below this line
+    if (n <= 0) return 0;
+    return arr[n - 1] + sum(arr, n - 1);
+    // Only change code above this line
+}
+console.log(sum([2, 3, 4, 5], 3));
