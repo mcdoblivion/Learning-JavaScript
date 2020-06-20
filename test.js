7b42cd26838bd52e3a662a777b8425b188d2c6a4
mcd_oblivion <mcd.oblivion@gmail.com>
2020-06-20 10:18:09 +0700

    Multiple Identical Options in Switch Statements

--- test.js
+++ test.js
@@ -1,22 +1,25 @@
-function switchOfStuff(val) {
+function sequentialSizes(val) {
     var answer = "";
     // Only change code below this line
     switch (val) {
-        case "a":
-            answer = "apple";
+        case 1:
+        case 2:
+        case 3:
+            answer = "Low";
             break;
-        case "b":
-            answer = "bird";
+        case 4:
+        case 5:
+        case 6:
+            answer = "Mid";
             break;
-        case "c":
-            answer = "cat";
-            break;
-        default:
-            answer = "stuff";
+        case 7:
+        case 8:
+        case 9:
+            answer = "High";
     }
 
     // Only change code above this line
     return answer;
 }
 
-switchOfStuff(1);
+sequentialSizes(1);
