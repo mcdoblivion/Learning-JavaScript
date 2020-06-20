2e66c6f7bf67aa6154afba69a1f61304cab75e48
mcd_oblivion <mcd.oblivion@gmail.com>
2020-06-20 15:17:45 +0700

    Profile Lookup

--- test.js
+++ test.js
@@ -1,7 +1,47 @@
-function sum(arr, n) {
+// Setup
+var contacts = [
+    {
+        firstName: "Akira",
+        lastName: "Laine",
+        number: "0543236543",
+        likes: ["Pizza", "Coding", "Brownie Points"],
+    },
+    {
+        firstName: "Harry",
+        lastName: "Potter",
+        number: "0994372684",
+        likes: ["Hogwarts", "Magic", "Hagrid"],
+    },
+    {
+        firstName: "Sherlock",
+        lastName: "Holmes",
+        number: "0487345643",
+        likes: ["Intriguing Cases", "Violin"],
+    },
+    {
+        firstName: "Kristian",
+        lastName: "Vos",
+        number: "unknown",
+        likes: ["JavaScript", "Gaming", "Foxes"],
+    },
+];
+
+function lookUpProfile(name, prop) {
     // Only change code below this line
-    if (n <= 0) return 0;
-    return arr[n - 1] + sum(arr, n - 1);
+    var hasName = false;
+    for (var i = 0; i < contacts.length; i++) {
+        if (name == contacts[i].firstName) {
+            if (contacts[i].hasOwnProperty(prop)) {
+                return contacts[i][prop];
+            }
+            hasName = true;
+        }
+    }
+    if (hasName) {
+        return "No such property";
+    }
+    return "No such contact";
     // Only change code above this line
 }
-console.log(sum([2, 3, 4, 5], 3));
+
+lookUpProfile("Akira", "likes");
