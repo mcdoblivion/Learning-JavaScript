869a3a086ba21603824212febb79412c864e2a57
mcd_oblivion <mcd.oblivion@gmail.com>
2020-06-20 10:10:40 +0700

    Golf Code

--- test.js
+++ test.js
@@ -1,18 +1,32 @@
-function testSize(num) {
+var names = [
+    "Hole-in-one!",
+    "Eagle",
+    "Birdie",
+    "Par",
+    "Bogey",
+    "Double Bogey",
+    "Go Home!",
+];
+function golfScore(par, strokes) {
     // Only change code below this line
-    if (num < 5) {
-        return "Tiny";
-    } else if (num < 10) {
-        return "Small";
-    } else if (num < 15) {
-        return "Medium";
-    } else if (num < 20) {
-        return "Large";
+    if (strokes == 1) {
+        return "Hole-in-one!";
+    } else if (strokes <= par - 2) {
+        return "Eagle";
+    } else if (strokes == par - 1) {
+        return "Birdie";
+    } else if (strokes == par) {
+        return "Par";
+    } else if (strokes == par + 1) {
+        return "Bogey";
+    } else if (strokes == par + 2) {
+        return "Double Bogey";
     } else {
-        return "Huge";
+        return "Go Home!";
     }
 
+    return "Change Me";
     // Only change code above this line
 }
 
-testSize(7);
+golfScore(5, 4);
