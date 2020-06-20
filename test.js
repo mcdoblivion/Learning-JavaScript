6d3c3546cbf1d400d08baefc1f27dfe20773a604
mcd_oblivion <mcd.oblivion@gmail.com>
2020-06-20 10:35:24 +0700

    Counting Cards

--- test.js
+++ test.js
@@ -1,14 +1,25 @@
-// Setup
-function abTest(a, b) {
+var count = 0;
+
+function cc(card) {
     // Only change code below this line
-
-    if (a < 0 || b < 0) {
-        return;
+    if (card >= 2 && card <= 6) {
+        count++;
+    } else if (card < 10) {
+    } else {
+        count--;
     }
-
+    var result;
+    if (count > 0) {
+        result = count + " Bet";
+    } else {
+        result = count + " Hold";
+    }
+    return result;
     // Only change code above this line
-
-    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
 }
 
-abTest(2, 2);
+cc(2);
+cc(3);
+cc(7);
+cc("K");
+cc("A");
