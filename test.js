b949f09076e62efcc02682c52554c74ac63a6341
mcd_oblivion <mcd.oblivion@gmail.com>
2020-06-20 15:24:46 +0700

    Generate Random Whole Numbers within a Range

--- test.js
+++ test.js
@@ -1,7 +1,5 @@
-var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
-
-function randomWholeNum() {
+function randomRange(myMin, myMax) {
     // Only change code below this line
-
-    return Math.floor(Math.random() * 10);
+    return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+    // Only change code above this line
 }
