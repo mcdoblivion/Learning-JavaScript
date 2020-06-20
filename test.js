0f76cd2d2cba5fb6d80f4c9a325df11140894036
mcd_oblivion <mcd.oblivion@gmail.com>
2020-06-20 10:03:46 +0700

    Chaining If Else Statements

--- test.js
+++ test.js
@@ -1,11 +1,18 @@
-function orderMyLogic(val) {
-    if (val < 5) {
-        return "Less than 5";
-    } else if (val < 10) {
-        return "Less than 10";
+function testSize(num) {
+    // Only change code below this line
+    if (num < 5) {
+        return "Tiny";
+    } else if (num < 10) {
+        return "Small";
+    } else if (num < 15) {
+        return "Medium";
+    } else if (num < 20) {
+        return "Large";
     } else {
-        return "Greater than or equal to 10";
+        return "Huge";
     }
+
+    // Only change code above this line
 }
 
-orderMyLogic(7);
+testSize(7);
