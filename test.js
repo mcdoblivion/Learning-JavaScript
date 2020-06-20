8018f91bd7c305811c621c8230b627ce15f80626
mcd_oblivion <mcd.oblivion@gmail.com>
2020-06-20 10:39:27 +0700

    Build JavaScript Objects

--- test.js
+++ test.js
@@ -1,25 +1,8 @@
-var count = 0;
-
-function cc(card) {
+var myDog = {
     // Only change code below this line
-    if (card >= 2 && card <= 6) {
-        count++;
-    } else if (card < 10) {
-    } else {
-        count--;
-    }
-    var result;
-    if (count > 0) {
-        result = count + " Bet";
-    } else {
-        result = count + " Hold";
-    }
-    return result;
+    name: "My Dog",
+    legs: 4,
+    tails: 1,
+    friends: ["Cat", "Other dog"],
     // Only change code above this line
-}
-
-cc(2);
-cc(3);
-cc(7);
-cc("K");
-cc("A");
+};
