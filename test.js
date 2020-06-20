29b427e490efd0cb9aac7cecbe23dc7a58026add
mcd_oblivion <mcd.oblivion@gmail.com>
2020-06-20 10:49:55 +0700

    Updating Object Properties

--- test.js
+++ test.js
@@ -1,11 +1,10 @@
 // Setup
-var testObj = {
-    12: "Namath",
-    16: "Montana",
-    19: "Unitas",
-};
-
-// Only change code below this line
-
-var playerNumber = 16; // Change this line
-var player = testObj[playerNumber]; // Change this line
+var myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  
+  // Only change code below this line
+  myDog.name = "Happy Coder";
