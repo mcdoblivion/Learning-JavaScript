5c3738cecd9236acee7e663d9e3442bd1379f06c
mcd_oblivion <mcd.oblivion@gmail.com>
2020-06-20 17:12:02 +0700

    Use the Rest Parameter with Function Parameters

--- ES6Challenges.js
+++ ES6Challenges.js
@@ -1,3 +1,3 @@
-// Only change code below this line
-const increment = (number, value = 1) => number + value;
-// Only change code above this line
+const sum = (...args) => {
+    return args.reduce((a, b) => a + b, 0);
+};
