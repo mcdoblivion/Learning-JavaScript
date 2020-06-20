ee7f7c0be2ecef7547584f06540bd075d78ef787
mcd_oblivion <mcd.oblivion@gmail.com>
2020-06-20 15:02:16 +0700

    Iterate with JavaScript Do...While Loops

--- test.js
+++ test.js
@@ -1,17 +1,8 @@
-function multiplyAll(arr) {
-    var product = 1;
-    // Only change code below this line
-    for (var i = 0; i < arr.length; i++) {
-        for (var j = 0; j < arr[i].length; j++) {
-            product *= arr[i][j];
-        }
-    }
-    // Only change code above this line
-    return product;
-}
+// Setup
+var myArray = [];
+var i = 10;
 
-multiplyAll([
-    [1, 2],
-    [3, 4],
-    [5, 6, 7],
-]);
+// Only change code below this line
+do {
+    myArray.push(i++);
+} while (i < 11);
