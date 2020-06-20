241e531e8cc3ecec116ba3983fdd16c3be9f4a20
mcd_oblivion <mcd.oblivion@gmail.com>
2020-06-20 10:51:50 +0700

    Delete Properties from a JavaScript Object

--- test.js
+++ test.js
@@ -3,8 +3,9 @@ var myDog = {
     "name": "Happy Coder",
     "legs": 4,
     "tails": 1,
-    "friends": ["freeCodeCamp Campers"]
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
   };
   
   // Only change code below this line
-  myDog.bark = "woof";
+delete myDog.tails;  
