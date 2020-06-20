66969aa55de24f9af22b185800ca471493b06e75
mcd_oblivion <mcd.oblivion@gmail.com>
2020-06-20 16:51:25 +0700

    Prevent Object Mutation

--- ES6Challenges.js
+++ ES6Challenges.js
@@ -1,12 +1,18 @@
-const s = [5, 7, 2];
-function editInPlace() {
-    "use strict";
+function freezeObj() {
+    'use strict';
+    const MATH_CONSTANTS = {
+      PI: 3.14
+    };
     // Only change code below this line
-
-    // Using s = [2, 5, 7] would be invalid
-    s[0] = 2;
-    s[2] = 7;
-    s[1] = 5;
+  
+    Object.freeze(MATH_CONSTANTS);
     // Only change code above this line
-}
-editInPlace();
+    try {
+      MATH_CONSTANTS.PI = 99;
+    } catch(ex) {
+      console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
+  }
+  const PI = freezeObj();
+  
