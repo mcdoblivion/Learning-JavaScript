368624c479c3b70921940ee819fb1eab3cb09187
mcd_oblivion <mcd.oblivion@gmail.com>
2020-06-21 22:38:49 +0700

    Find One or More Criminals in a Hunt

--- RegularExpressionChallenges.js
+++ RegularExpressionChallenges.js
@@ -1,4 +1,1 @@
-let text = "<h1>Winter is coming</h1>";
-let myRegex = /<.*?>/; // Change this line
-let result = text.match(myRegex);
-console.log(result);
+let reCriminals = /c+/gi; // Change this line
