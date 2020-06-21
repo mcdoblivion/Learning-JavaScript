d479cdc0080994ef648ecb0a5097607d95ba9956
mcd_oblivion <mcd.oblivion@gmail.com>
2020-06-21 11:23:03 +0700

    Write Concise Declarative Functions with ES6

--- ES6Challenges.js
+++ ES6Challenges.js
@@ -1,10 +1,10 @@
-const createPerson = (name, age, gender) => {
-    "use strict";
-    // Only change code below this line
-    return {
-        name,
-        age,
-        gender,
-    };
-    // Only change code above this line
+// Only change code below this line
+const bicycle = {
+    gear: 2,
+    setGear(newGear) {
+        this.gear = newGear;
+    },
 };
+// Only change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
