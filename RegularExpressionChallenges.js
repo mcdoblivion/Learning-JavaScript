c7ef968662b8b3ea7227b6e52bc317e42f87fa47
mcd_oblivion <mcd.oblivion@gmail.com>
2020-06-21 22:12:52 +0700

    Match Single Character with Multiple Possibilities

--- RegularExpressionChallenges.js
+++ RegularExpressionChallenges.js
@@ -1,3 +1,4 @@
-let exampleStr = "Let's have fun with regular expressions!";
-let unRegex = /.un/; // Change this line
-let result = unRegex.test(exampleStr);
+let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[ueoai]/gi; // Change this line
+let result = quoteSample.match(vowelRegex); // Change this line
+console.log(result);
