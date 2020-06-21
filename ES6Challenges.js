7223331cddbf42a9472d6c03749e9141ea9ddcbd
mcd_oblivion <mcd.oblivion@gmail.com>
2020-06-21 10:55:30 +0700

    Destructuring Assignment-Assign Variables fr Arr

--- ES6Challenges.js
+++ ES6Challenges.js
@@ -1,13 +1,3 @@
-const LOCAL_FORECAST = {
-    yesterday: { low: 61, high: 75 },
-    today: { low: 64, high: 77 },
-    tomorrow: { low: 68, high: 80 },
-};
-
+let a = 8, b = 6;
 // Only change code below this line
-
-const {
-    today: { low: lowToday, high: highToday }
-} = LOCAL_FORECAST;
-
-// Only change code above this line
+[a, b] = [b, a];
