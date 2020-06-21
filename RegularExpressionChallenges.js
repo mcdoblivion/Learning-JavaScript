154cf1bf6c218576d3a403b398b26039c9b8405b
mcd_oblivion <mcd.oblivion@gmail.com>
2020-06-21 22:27:27 +0700

    Match Characters that Occur One or More Times

--- RegularExpressionChallenges.js
+++ RegularExpressionChallenges.js
@@ -1,3 +1,3 @@
-let quoteSample = "3 blind mice.";
-let myRegex = /[^ueoai0-9]/gi; // Change this line
-let result = quoteSample.match(myRegex); // Change this line
+let difficultSpelling = "Mississippi";
+let myRegex = /s+/g; // Change this line
+let result = difficultSpelling.match(myRegex);
