30de03e55e2b9da2e5709fbdf51d816e9988e0c1
mcd_oblivion <mcd.oblivion@gmail.com>
2020-06-21 22:15:18 +0700

    Match Letters of the Alphabet

--- RegularExpressionChallenges.js
+++ RegularExpressionChallenges.js
@@ -1,4 +1,4 @@
-let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
-let vowelRegex = /[ueoai]/gi; // Change this line
-let result = quoteSample.match(vowelRegex); // Change this line
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi; // Change this line
+let result = quoteSample.match(alphabetRegex); // Change this line
 console.log(result);
