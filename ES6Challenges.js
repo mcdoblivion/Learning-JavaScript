406a29f46d3f6c0e7334db2025ed54257ba54a49
mcd_oblivion <mcd.oblivion@gmail.com>
2020-06-21 17:13:40 +0700

    Use getters and setters to Control Access to Obj

--- ES6Challenges.js
+++ ES6Challenges.js
@@ -1,10 +1,20 @@
 // Only change code below this line
-class Vegetable {
-    constructor(name) {
-        this.name = name;
+class Thermostat {
+    constructor(fahrenheitTemperature) {
+        this.fahrenheitTemperature = fahrenheitTemperature;
+    }
+
+    get temperature() {
+        return ((this.fahrenheitTemperature - 32) * 5) / 9;
+    }
+
+    set temperature(celsiusTemperature) {
+        this.fahrenheitTemperature = celsiusTemperature * 1.8 + 32;
     }
 }
 // Only change code above this line
 
-const carrot = new Vegetable('carrot');
-console.log(carrot.name); // Should display 'carrot'
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
