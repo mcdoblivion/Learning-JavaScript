4cf70ebf669250453c5346700d74f6c15a9b62a2
mcd_oblivion <mcd.oblivion@gmail.com>
2020-06-21 10:44:09 +0700

    Destructuring Assignment-Assign Var fr Nested Obj

--- ES6Challenges.js
+++ ES6Challenges.js
@@ -1,11 +1,13 @@
-const HIGH_TEMPERATURES = {
-    yesterday: 75,
-    today: 77,
-    tomorrow: 80,
+const LOCAL_FORECAST = {
+    yesterday: { low: 61, high: 75 },
+    today: { low: 64, high: 77 },
+    tomorrow: { low: 68, high: 80 },
 };
 
 // Only change code below this line
 
-const {today: highToday, tomorrow: highTomorrow} = HIGH_TEMPERATURES;
+const {
+    today: { low: lowToday, high: highToday }
+} = LOCAL_FORECAST;
 
 // Only change code above this line
