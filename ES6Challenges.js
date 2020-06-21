5a418e706ce48a57883d9e3e4361f7103b435ae0
mcd_oblivion <mcd.oblivion@gmail.com>
2020-06-21 11:21:17 +0700

    Write Concise Object Literal Declarations

--- ES6Challenges.js
+++ ES6Challenges.js
@@ -1,19 +1,10 @@
-const result = {
-    success: ["max-length", "no-amd", "prefer-arrow-functions"],
-    failure: ["no-var", "var-on-top", "linebreak"],
-    skipped: ["id-blacklist", "no-dup-keys"],
-};
-function makeList(arr) {
+const createPerson = (name, age, gender) => {
     "use strict";
-
     // Only change code below this line
-    const resultDisplayArray = [];
-    for (let i = 0; i < arr.length; i++) {
-        resultDisplayArray.push(`<li class="text-warning">${arr[i]}</li>`);
-    }
+    return {
+        name,
+        age,
+        gender,
+    };
     // Only change code above this line
-
-    return resultDisplayArray;
-}
-
-const resultDisplayArray = makeList(result.failure);
+};
