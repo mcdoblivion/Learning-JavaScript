082fc10413368042c2b33f4722301cd6e6c68f33
mcd_oblivion <mcd.oblivion@gmail.com>
2020-06-21 17:23:24 +0700

    Use export to Share a Code Block

--- ES6Challenges.js
+++ ES6Challenges.js
@@ -1,20 +1,9 @@
-// Only change code below this line
-class Thermostat {
-    constructor(fahrenheitTemperature) {
-        this.fahrenheitTemperature = fahrenheitTemperature;
-    }
+const uppercaseString = (string) => {
+    return string.toUpperCase();
+};
 
-    get temperature() {
-        return ((this.fahrenheitTemperature - 32) * 5) / 9;
-    }
+const lowercaseString = (string) => {
+    return string.toLowerCase();
+};
 
-    set temperature(celsiusTemperature) {
-        this.fahrenheitTemperature = celsiusTemperature * 1.8 + 32;
-    }
-}
-// Only change code above this line
-
-const thermos = new Thermostat(76); // Setting in Fahrenheit scale
-let temp = thermos.temperature; // 24.44 in Celsius
-thermos.temperature = 26;
-temp = thermos.temperature; // 26 in Celsius
+export {uppercaseString, lowercaseString};
