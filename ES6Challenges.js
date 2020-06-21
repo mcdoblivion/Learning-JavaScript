37b2b7d479c91c7dd6d6c6dbca5911d58cea1d13
mcd_oblivion <mcd.oblivion@gmail.com>
2020-06-21 17:51:36 +0700

    Handle a Rejected Promise with catch

--- ES6Challenges.js
+++ ES6Challenges.js
@@ -1,6 +1,6 @@
 const makeServerRequest = new Promise((resolve, reject) => {
-    // responseFromServer is set to true to represent a successful response from a server
-    let responseFromServer = true;
+    // responseFromServer is set to false to represent an unsuccessful response from a server
+    let responseFromServer = false;
 
     if (responseFromServer) {
         resolve("We got the data");
@@ -9,6 +9,10 @@ const makeServerRequest = new Promise((resolve, reject) => {
     }
 });
 
-makeServerRequest.then(result => {
+makeServerRequest.then((result) => {
     console.log(result);
+});
+
+makeServerRequest.catch(error => {
+    console.log(error);
 })
