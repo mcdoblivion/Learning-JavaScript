6a3441f69489c783586f9c3ee8cfe887f8baa2c0
mcd_oblivion <mcd.oblivion@gmail.com>
2020-06-21 22:04:20 +0700

    Find More Than the First Match

--- RegularExpressionChallenges.js
+++ RegularExpressionChallenges.js
@@ -1,4 +1,4 @@
-let extractStr = "Extract the word 'coding' from this string.";
-let codingRegex = /coding/; // Change this line
-let result = extractStr.match(codingRegex); // Change this line
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /twinkle/gi; // Change this line
+let result = twinkleStar.match(starRegex); // Change this line
 console.log(result);
