5635089cb4ad4a080e1b8350bb9946227eb01d12
mcd_oblivion <mcd.oblivion@gmail.com>
2020-06-21 17:35:57 +0700

    Create an Export Fallback with export default

--- ES6Challenges.js
+++ ES6Challenges.js
@@ -1,5 +1,3 @@
-import * as stringFunctions from './string_functions.js';
-// Only change code above this line
-
-stringFunctions.uppercaseString("hello");
-stringFunctions.lowercaseString("WORLD!");
+export default function subtract(x, y) {
+    return x - y;
+}
