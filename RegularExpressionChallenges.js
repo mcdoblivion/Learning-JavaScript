bc25e617b39bc0f15b7683e17d5cb03a7c75ff64
mcd_oblivion <mcd.oblivion@gmail.com>
2020-06-28 22:47:56 +0700

    Restrict Possible Usernames

--- RegularExpressionChallenges.js
+++ RegularExpressionChallenges.js
@@ -1,3 +1,4 @@
-let movieName = "2001: A Space Odyssey";
-let noNumRegex = /\D/g; // Change this line
-let result = movieName.match(noNumRegex).length;
+let username = "Z97";
+let userCheck = /^[a-z]([a-z]+\d*|[\d][\d])$/i; // Change this line
+let result = userCheck.test(username);
+console.log(username.match(userCheck));
