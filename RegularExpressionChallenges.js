417368567017d051def00f44e92fa35af39e5bde
mcd_oblivion <mcd.oblivion@gmail.com>
2020-06-28 21:58:01 +0700

    Match All Non-Numbers

--- RegularExpressionChallenges.js
+++ RegularExpressionChallenges.js
@@ -1,29 +1,3 @@
 let movieName = "2001: A Space Odyssey";
-let numRegex = /\d/g; // Change this line
-let result = movieName.match(numRegex).length;
-switch (result) {
-    case 0:
-        console.log("zero");
-        break;
-    case 1:
-        console.log("one");
-    case 2:
-        console.log("two");
-        break;
-    case 3:
-        console.log("three");
-    case 4:
-        console.log("four");
-        break;
-    case 5:
-        console.log("five");
-    case 6:
-        console.log("six");
-        break;
-    case 7:
-        console.log("seven");
-    case 8:
-        console.log("eight");
-    case 9:
-        console.log("nine");
-}
+let noNumRegex = /\D/g; // Change this line
+let result = movieName.match(noNumRegex).length;
