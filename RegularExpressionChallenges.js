90a253057a388804512c95bce7284662e4e7df18
mcd_oblivion <mcd.oblivion@gmail.com>
2020-06-28 21:57:22 +0700

    Match All Numbers

--- RegularExpressionChallenges.js
+++ RegularExpressionChallenges.js
@@ -1,3 +1,29 @@
-let quoteSample = "The five boxing wizards jump quickly.";
-let nonAlphabetRegex = /\W/g; // Change this line
-let result = quoteSample.match(nonAlphabetRegex).length;
+let movieName = "2001: A Space Odyssey";
+let numRegex = /\d/g; // Change this line
+let result = movieName.match(numRegex).length;
+switch (result) {
+    case 0:
+        console.log("zero");
+        break;
+    case 1:
+        console.log("one");
+    case 2:
+        console.log("two");
+        break;
+    case 3:
+        console.log("three");
+    case 4:
+        console.log("four");
+        break;
+    case 5:
+        console.log("five");
+    case 6:
+        console.log("six");
+        break;
+    case 7:
+        console.log("seven");
+    case 8:
+        console.log("eight");
+    case 9:
+        console.log("nine");
+}
