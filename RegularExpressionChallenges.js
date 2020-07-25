dc172af5269dec9c1cf7a72050b423748158e4d4
mcd_oblivion <mcd.oblivion@gmail.com>
2020-07-25 11:06:09 +0700

    Use Capture Groups to Search and Replace

--- RegularExpressionChallenges.js
+++ RegularExpressionChallenges.js
@@ -1,3 +1,4 @@
-let repeatNum = "42 42 42";
-let reRegex = /^(\d+)(\s)\1\2\1$/; // Change this line
-let result = reRegex.test(repeatNum);
+let str = "one two three";
+let fixRegex = /(\w+)\s(\w+)\s(\w+)/; // Change this line
+let replaceText = "$3 $2 $1"; // Change this line
+let result = str.replace(fixRegex, replaceText);
