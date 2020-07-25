3688a3e92c54f60d6269dfb2d62605380b1e5e6e
mcd_oblivion <mcd.oblivion@gmail.com>
2020-07-25 16:43:45 +0700

    Catch Unclosed Parentheses,Brackets,Braces,Quotes

--- Debugging.js
+++ Debugging.js
@@ -1,4 +1,4 @@
-let receivables = 10;
-let payables = 8;
-let netWorkingCapital = receivables - payables;
-console.log(`Net working capital is: ${netWorkingCapital}`);
+let myArray = [1, 2, 3];
+    let arraySum = myArray.reduce((previous, current) =>  (previous + current));
+    console.log(`Sum of array values is: ${arraySum}`);
+    
