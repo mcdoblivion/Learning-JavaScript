e839e1327aef1c91ddf433c3894ec7277cddca6d
mcd_oblivion <mcd.oblivion@gmail.com>
2020-07-25 16:50:42 +0700

    Catch Missing ( and ) After a Function Call

--- Debugging.js
+++ Debugging.js
@@ -1,11 +1,8 @@
-let x = 7;
-let y = 9;
-let result = "to come";
-
-if ((x == y)) {
-    result = "Equal!";
-} else {
-    result = "Not equal!";
+function getNine() {
+    let x = 6;
+    let y = 3;
+    return x + y;
 }
 
+let result = getNine();
 console.log(result);
