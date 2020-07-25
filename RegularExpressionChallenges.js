beffb29db4db8233b661d13c75676d3b100ff470
mcd_oblivion <mcd.oblivion@gmail.com>
2020-07-25 11:11:33 +0700

    Remove Whitespace from Start and End

--- RegularExpressionChallenges.js
+++ RegularExpressionChallenges.js
@@ -1,4 +1,3 @@
-let str = "one two three";
-let fixRegex = /(\w+)\s(\w+)\s(\w+)/; // Change this line
-let replaceText = "$3 $2 $1"; // Change this line
-let result = str.replace(fixRegex, replaceText);
+let hello = "   Hello, World!  ";
+let wsRegex = /^\s+|\s+$/g; // Change this line
+let result = hello.replace(wsRegex, ''); // Change this line
