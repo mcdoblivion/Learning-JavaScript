6746f7a6a15b78c2045bab14a495435aedf0d6f3
mcd_oblivion <mcd.oblivion@gmail.com>
2020-07-25 09:15:41 +0700

    Specify Upper and Lower Number of Matches

--- RegularExpressionChallenges.js
+++ RegularExpressionChallenges.js
@@ -1,3 +1,3 @@
-let sample = "Whitespace is important in separating words";
-let countNonWhiteSpace = /\S/g; // Change this line
-let result = sample.match(countNonWhiteSpace);
+let ohStr = "Ohhh no";
+let ohRegex = /Oh{3,6}\s*no/g; // Change this line
+let result = ohRegex.test(ohStr);
