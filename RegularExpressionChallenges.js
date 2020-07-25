bf487305b41cf0661d63da43d2b10ad5696b966b
mcd_oblivion <mcd.oblivion@gmail.com>
2020-07-25 09:10:27 +0700

    Match Non-Whitespace Characters

--- RegularExpressionChallenges.js
+++ RegularExpressionChallenges.js
@@ -1,3 +1,3 @@
 let sample = "Whitespace is important in separating words";
-let countWhiteSpace = /\s/g; // Change this line
-let result = sample.match(countWhiteSpace);
+let countNonWhiteSpace = /\S/g; // Change this line
+let result = sample.match(countNonWhiteSpace);
