77f9e8db8ad1141074c449b72d81cf70476c2cde
mcd_oblivion <mcd.oblivion@gmail.com>
2020-07-25 16:32:34 +0700

    Catch Misspelled Variable and Function Names

--- Debugging.js
+++ Debugging.js
@@ -1,6 +1,4 @@
-let seven = 7;
-let three = "3";
-console.log(seven + three);
-// Only change code below this line
-console.log(typeof seven);
-console.log(typeof three);
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = receivables - payables;
+console.log(`Net working capital is: ${netWorkingCapital}`);
