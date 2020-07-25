c5bc64d95381386f3da84e4ba80c69e902519b1d
mcd_oblivion <mcd.oblivion@gmail.com>
2020-07-25 17:02:29 +0700

    Use Caution When Reinitializing Var Inside a Loop

--- Debugging.js
+++ Debugging.js
@@ -1,11 +1,20 @@
-function countToFive() {
-    let firstFive = "12345";
-    let len = firstFive.length;
-    // Only change code below this line
-    for (let i = 0; i < len; i++) {
-        // Only change code above this line
-        console.log(firstFive[i]);
+function zeroArray(m, n) {
+    // Creates a 2-D array with m rows and n columns of zeroes
+    let newArray = [];
+    let row = [];
+    for (let i = 0; i < m; i++) {
+        // Adds the m-th row into newArray
+        
+        for (let j = 0; j < n; j++) {
+            // Pushes n zeroes into the current row to create the columns
+            row.push(0);
+        }
+        // Pushes the current row, which now has n zeroes in it, to the array
+        newArray.push(row);
+        row = [];
     }
+    return newArray;
 }
 
-countToFive();
+let matrix = zeroArray(3, 2);
+console.log(matrix);
