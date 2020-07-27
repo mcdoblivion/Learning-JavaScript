303abaa9726addaa0d9215802307322e0c5ecd5d
mcd_oblivion <mcd.oblivion@gmail.com>
2020-07-27 09:36:57 +0700

    Add Items Using splice()

--- BasicDataStructure.js
+++ BasicDataStructure.js
@@ -1,5 +1,16 @@
-const arr = [2, 4, 5, 1, 7, 5, 2, 1];
-// Only change code below this line
-arr.splice(1, 4);
-// Only change code above this line
-console.log(arr);
+function htmlColorNames(arr) {
+    // Only change code below this line
+    arr.splice(0, 2, 'DarkSalmon', 'BlanchedAlmond');
+    // Only change code above this line
+    return arr;
+}
+
+console.log(
+    htmlColorNames([
+        "DarkGoldenRod",
+        "WhiteSmoke",
+        "LavenderBlush",
+        "PaleTurquoise",
+        "FireBrick",
+    ])
+);
