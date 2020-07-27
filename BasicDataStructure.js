1e3986fc2d23d6b0270e3f0382aaa0cc9494d29c
mcd_oblivion <mcd.oblivion@gmail.com>
2020-07-27 09:41:51 +0700

    Copy Array Items Using slice()

--- BasicDataStructure.js
+++ BasicDataStructure.js
@@ -1,16 +1,10 @@
-function htmlColorNames(arr) {
+function forecast(arr) {
     // Only change code below this line
-    arr.splice(0, 2, 'DarkSalmon', 'BlanchedAlmond');
-    // Only change code above this line
+    arr = arr.slice(2, 4);
     return arr;
 }
 
+// Only change code above this line
 console.log(
-    htmlColorNames([
-        "DarkGoldenRod",
-        "WhiteSmoke",
-        "LavenderBlush",
-        "PaleTurquoise",
-        "FireBrick",
-    ])
+    forecast(["cold", "rainy", "warm", "sunny", "cool", "thunderstorms"])
 );
