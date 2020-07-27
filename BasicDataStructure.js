167aa9da1e2123ec8d920fe5da7ddb9b9319f0f3
mcd_oblivion <mcd.oblivion@gmail.com>
2020-07-27 09:50:27 +0700

    Copy an Array with the Spread Operator

--- BasicDataStructure.js
+++ BasicDataStructure.js
@@ -1,10 +1,12 @@
-function forecast(arr) {
-    // Only change code below this line
-    arr = arr.slice(2, 4);
-    return arr;
+function copyMachine(arr, num) {
+    let newArr = [];
+    while (num >= 1) {
+        // Only change code below this line
+        newArr.push([...arr]);
+        // Only change code above this line
+        num--;
+    }
+    return newArr;
 }
 
-// Only change code above this line
-console.log(
-    forecast(["cold", "rainy", "warm", "sunny", "cool", "thunderstorms"])
-);
+console.log(copyMachine([true, false, true], 2));
