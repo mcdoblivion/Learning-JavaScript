98a3acdf3162c77ecfbd8e2f40edb15cb8b9ab78
mcd_oblivion <mcd.oblivion@gmail.com>
2020-07-27 09:55:04 +0700

    Combine Arrays with the Spread Operator

--- BasicDataStructure.js
+++ BasicDataStructure.js
@@ -1,12 +1,7 @@
-function copyMachine(arr, num) {
-    let newArr = [];
-    while (num >= 1) {
-        // Only change code below this line
-        newArr.push([...arr]);
-        // Only change code above this line
-        num--;
-    }
-    return newArr;
+function spreadOut() {
+    let fragment = ["to", "code"];
+    let sentence = ['learning', ...fragment, 'is', 'fun']; // Change this line
+    return sentence;
 }
 
-console.log(copyMachine([true, false, true], 2));
+console.log(spreadOut());
