f38f80e4fceeca285c3c4e67d1a5f3660b6aa23f
mcd_oblivion <mcd.oblivion@gmail.com>
2020-07-29 09:08:33 +0700

    Iterate Through the Keys of an Obj with a for...in

--- BasicDataStructure.js
+++ BasicDataStructure.js
@@ -1,26 +1,9 @@
-let users = {
-    Alan: {
-        age: 27,
-        online: true,
-    },
-    Jeff: {
-        age: 32,
-        online: true,
-    },
-    Sarah: {
-        age: 48,
-        online: true,
-    },
-    Ryan: {
-        age: 19,
-        online: true,
-    },
-};
-
-function isEveryoneHere(obj) {
+function countOnline(usersObj) {
     // Only change code below this line
-    return "Alan" in obj && "Jeff" in obj && "Sarah" in obj && "Ryan" in obj;
+    let count = 0;
+    for (let user in usersObj) {
+        if (usersObj[user].online) count++;
+    }
+    return count;
     // Only change code above this line
 }
-
-console.log(isEveryoneHere(users));
