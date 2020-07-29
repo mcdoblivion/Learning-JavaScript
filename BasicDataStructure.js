88aace4e8494fbd587eb7f61c2f4c03fb2ba1a33
mcd_oblivion <mcd.oblivion@gmail.com>
2020-07-29 08:33:36 +0700

    Access Property Names with Bracket Notation

--- BasicDataStructure.js
+++ BasicDataStructure.js
@@ -1,14 +1,16 @@
-let userActivity = {
-    id: 23894201352,
-    date: "January 1, 2017",
-    data: {
-        totalUsers: 51,
-        online: 42,
-    },
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28,
+    bananas: 13,
+    grapes: 35,
+    strawberries: 27,
 };
 
-// Only change code below this line
-userActivity.data.online = 45;
-// Only change code above this line
+function checkInventory(scannedItem) {
+    // Only change code below this line
+    return foods[scannedItem];
+    // Only change code above this line
+}
 
-console.log(userActivity);
+console.log(checkInventory("apples"));
