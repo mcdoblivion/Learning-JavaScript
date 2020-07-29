e99d32e4bcce8ee4fde4b9dc4fd3d0f50c2c48d1
mcd_oblivion <mcd.oblivion@gmail.com>
2020-07-29 08:30:03 +0700

    Modify an Object Nested Within an Object

--- BasicDataStructure.js
+++ BasicDataStructure.js
@@ -1,13 +1,14 @@
-let foods = {
-    apples: 25,
-    oranges: 32,
-    plums: 28,
+let userActivity = {
+    id: 23894201352,
+    date: "January 1, 2017",
+    data: {
+        totalUsers: 51,
+        online: 42,
+    },
 };
 
 // Only change code below this line
-foods.bananas = 13;
-foods['grapes'] = 35;
-foods.strawberries = 27;
+userActivity.data.online = 45;
 // Only change code above this line
 
-console.log(foods);
+console.log(userActivity);
