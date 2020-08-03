593480eb83b98a497eb8be52d6a3aec1be5a9df2
mcd_oblivion <mcd.oblivion@gmail.com>
2020-08-03 10:22:51 +0700

    Return Largest Numbers in Arrays

--- BasicAlgorithmScripting.js
+++ BasicAlgorithmScripting.js
@@ -1,10 +1,18 @@
-function findLongestWordLength(str) {
-    let arr = str.split(" ");
-    let maxLength = 0;
+function largestOfFour(arr) {
+    let arr1 = [];
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i].length > maxLength) maxLength = arr[i].length;
+        let max = arr[i][0];
+        for (let j = 1; j < arr[i].length; j++) {
+            if (arr[i][j] > max) max = arr[i][j];
+        }
+        arr1.push(max);
     }
-    return maxLength;
+    return arr1;
 }
 
-findLongestWordLength("The quick brown fox jumped over the lazy dog");
+largestOfFour([
+    [4, 5, 1, 3],
+    [13, 27, 18, 26],
+    [32, 35, 37, 39],
+    [1000, 1001, 857, 1],
+]);
