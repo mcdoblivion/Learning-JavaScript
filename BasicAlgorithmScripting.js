0b64c6eaf9451bdf5d819ac1e8571c9c34a06b9b
mcd_oblivion <mcd.oblivion@gmail.com>
2020-08-05 08:14:20 +0700

    Confirm the Ending

--- BasicAlgorithmScripting.js
+++ BasicAlgorithmScripting.js
@@ -1,18 +1,5 @@
-function largestOfFour(arr) {
-    let arr1 = [];
-    for (let i = 0; i < arr.length; i++) {
-        let max = arr[i][0];
-        for (let j = 1; j < arr[i].length; j++) {
-            if (arr[i][j] > max) max = arr[i][j];
-        }
-        arr1.push(max);
-    }
-    return arr1;
+function confirmEnding(str, target) {
+    return str.substring(str.length - target.length, str.length) == target;
 }
 
-largestOfFour([
-    [4, 5, 1, 3],
-    [13, 27, 18, 26],
-    [32, 35, 37, 39],
-    [1000, 1001, 857, 1],
-]);
+confirmEnding("Bastian", "n");
