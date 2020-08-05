f7d38efc854ebd2ff9a37bd358d824cde0ba63ff
mcd_oblivion <mcd.oblivion@gmail.com>
2020-08-05 08:29:52 +0700

    Boo who

--- BasicAlgorithmScripting.js
+++ BasicAlgorithmScripting.js
@@ -1,8 +1,5 @@
-function findElement(arr, func) {
-    for (let i = 0; i < arr.length; i++) {
-        if (func(arr[i])) return arr[i];
-    }
-    return undefined;
+function booWho(bool) {
+    return typeof(bool) == "boolean";
 }
 
-findElement([1, 2, 3, 4], (num) => num % 2 === 0);
+booWho(null);
