4d1ef405cb670a75795255215b967a3268442740
mcd_oblivion <mcd.oblivion@gmail.com>
2020-08-05 08:52:32 +0700

    Title Case a Sentence

--- BasicAlgorithmScripting.js
+++ BasicAlgorithmScripting.js
@@ -1,5 +1,12 @@
-function booWho(bool) {
-    return typeof(bool) == "boolean";
+function titleCase(str) {
+    str = str.toLowerCase();
+    let arr = str.split(" ");
+    let newStr = "";
+    for (let i = 0; i < arr.length; i ++) {
+        newStr = newStr + arr[i][0].toUpperCase() + arr[i].substring(1) + " ";
+    }
+    return newStr.trim();
 }
 
-booWho(null);
+titleCase("I'm a little tea pot");
+console.log(titleCase("I'm a little tea pot"));
