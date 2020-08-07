17d1bc6b0e0b41af3a6f713b4622a2c140c4cbd6
mcd_oblivion <mcd.oblivion@gmail.com>
2020-08-07 17:20:10 +0700

    Use an IIFE to Create a Module

--- ObjectOrientedProgrammingChallenges.js
+++ ObjectOrientedProgrammingChallenges.js
@@ -1,3 +1,14 @@
-(function () {
-    console.log("A cozy nest is ready");
+let funModule = (function () {
+    return {
+        isCuteMixin: function (obj) {
+            obj.isCute = function () {
+                return true;
+            };
+        },
+        singMixin: function (obj) {
+            obj.sing = function () {
+                console.log("Singing to an awesome tune");
+            };
+        },
+    };
 })();
