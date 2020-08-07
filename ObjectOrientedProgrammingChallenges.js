370041ae80b1c5e1b18b94ab12b84142d712d54f
mcd_oblivion <mcd.oblivion@gmail.com>
2020-08-07 09:13:13 +0700

    Define a Constructor Function

--- ObjectOrientedProgrammingChallenges.js
+++ ObjectOrientedProgrammingChallenges.js
@@ -1,9 +1,5 @@
-let dog = {
-    name: "Spot",
-    numLegs: 4,
-    sayLegs: function () {
-        return "This dog has " + this.numLegs + " legs.";
-    },
-};
-
-dog.sayLegs();
+function Dog() {
+    this.name = "a";
+    this.color = "black";
+    this.numLegs = 4;
+}
