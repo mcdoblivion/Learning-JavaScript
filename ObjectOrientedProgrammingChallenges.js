89f27211c2b7f5984df4524e0b3fa86c4b832df5
mcd_oblivion <mcd.oblivion@gmail.com>
2020-08-07 16:51:29 +0700

    Override Inherited Methods

--- ObjectOrientedProgrammingChallenges.js
+++ ObjectOrientedProgrammingChallenges.js
@@ -1,17 +1,20 @@
-function Animal() {}
-Animal.prototype.eat = function () {
-    console.log("nom nom nom");
+function Bird() {}
+
+Bird.prototype.fly = function () {
+    return "I am flying!";
 };
 
-function Dog() {}
+function Penguin() {}
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.constructor = Penguin;
 
 // Only change code below this line
-Dog.prototype = Object.create(Animal.prototype);
-Dog.prototype.constructor = Dog;
-Dog.prototype.bark = function () {
-    console.log("Woof!");
+
+Penguin.prototype.fly = function () {
+    return "Alas, this is a flightless bird.";
 };
 
 // Only change code above this line
 
-let beagle = new Dog();
+let penguin = new Penguin();
+console.log(penguin.fly());
