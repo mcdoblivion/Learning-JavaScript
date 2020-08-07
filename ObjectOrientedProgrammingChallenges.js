2146b6cf67fd72be5c5831d89038ee330066fc50
mcd_oblivion <mcd.oblivion@gmail.com>
2020-08-07 16:36:36 +0700

    Inherit Behaviors from a Supertype

--- ObjectOrientedProgrammingChallenges.js
+++ ObjectOrientedProgrammingChallenges.js
@@ -1,24 +1,13 @@
-function Cat(name) {
-    this.name = name;
-}
-
-Cat.prototype = {
-    constructor: Cat,
-};
-
-function Bear(name) {
-    this.name = name;
-}
-
-Bear.prototype = {
-    constructor: Bear,
-};
-
 function Animal() {}
 
 Animal.prototype = {
     constructor: Animal,
     eat: function () {
-        console.log("Eating");
+        console.log("nom nom nom");
     },
 };
+
+// Only change code below this line
+
+let duck = Object.create(Animal.prototype); // Change this line
+let beagle = Object.create(Animal.prototype); // Change this line
