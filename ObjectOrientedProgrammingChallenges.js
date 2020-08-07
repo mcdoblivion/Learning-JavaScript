52c9d602588de78f93ce801b202b4ac3ef74c0f7
mcd_oblivion <mcd.oblivion@gmail.com>
2020-08-07 16:59:52 +0700

    Mixin - Add Common Behavior Between Unrelated Objs

--- ObjectOrientedProgrammingChallenges.js
+++ ObjectOrientedProgrammingChallenges.js
@@ -1,20 +1,19 @@
-function Bird() {}
-
-Bird.prototype.fly = function () {
-    return "I am flying!";
+let bird = {
+    name: "Donald",
+    numLegs: 2,
 };
 
-function Penguin() {}
-Penguin.prototype = Object.create(Bird.prototype);
-Penguin.prototype.constructor = Penguin;
+let boat = {
+    name: "Warrior",
+    type: "race-boat",
+};
 
 // Only change code below this line
+let glideMixin = function(obj) {
+    obj.glide = function() {
+        console.log("Leu leu");
+    }
+}
 
-Penguin.prototype.fly = function () {
-    return "Alas, this is a flightless bird.";
-};
-
-// Only change code above this line
-
-let penguin = new Penguin();
-console.log(penguin.fly());
+glideMixin(bird);
+glideMixin(boat);
