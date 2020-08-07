cf2d42a71c20f2059735c34ae8557f35b2d844e1
mcd_oblivion <mcd.oblivion@gmail.com>
2020-08-07 10:07:22 +0700

    Understand Own Properties

--- ObjectOrientedProgrammingChallenges.js
+++ ObjectOrientedProgrammingChallenges.js
@@ -1,7 +1,13 @@
-function House(numBedrooms) {
-    this.numBedrooms = numBedrooms;
+function Bird(name) {
+    this.name = name;
+    this.numLegs = 2;
 }
 
+let canary = new Bird("Tweety");
+let ownProps = [];
 // Only change code below this line
-let myHouse = new House(4);
-myHouse instanceof House;
+for (let property in canary) {
+    if(canary.hasOwnProperty(property)) {
+        ownProps.push(property);
+    }
+}
