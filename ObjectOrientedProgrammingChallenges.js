69d3e16477da5271eb8f764d9b4c2a5562714994
mcd_oblivion <mcd.oblivion@gmail.com>
2020-08-07 10:19:21 +0700

    Understand the Constructor Property

--- ObjectOrientedProgrammingChallenges.js
+++ ObjectOrientedProgrammingChallenges.js
@@ -2,18 +2,8 @@ function Dog(name) {
     this.name = name;
 }
 
-Dog.prototype.numLegs = 4;
-
-let beagle = new Dog("Snoopy");
-
-let ownProps = [];
-let prototypeProps = [];
-
 // Only change code below this line
-for (let property in beagle) {
-    if (beagle.hasOwnProperty(property)) {
-        ownProps.push(property);
-    } else {
-        prototypeProps.push(property);
-    }
+function joinDogFraternity(candidate) {
+    if (candidate.constructor === Dog) return true;
+    return false;
 }
