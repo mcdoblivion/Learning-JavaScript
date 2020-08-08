898ccd98db4191a90845a3230118e8e864ea756a
mcd_oblivion <mcd.oblivion@gmail.com>
2020-08-08 08:03:38 +0700

    Understand the Hazards of Using Imperative Code

--- FunctionalProgrammingChallenges.js
+++ FunctionalProgrammingChallenges.js
@@ -1,27 +1,45 @@
-// Function that returns a string representing a cup of green tea
-const prepareGreenTea = () => "greenTea";
-
-// Function that returns a string representing a cup of black tea
-const prepareBlackTea = () => "blackTea";
-
-/*
-Given a function (representing the tea type) and number of cups needed, the
-following function returns an array of strings (each representing a cup of
-a specific type of tea).
-*/
-const getTea = (prepareTea, numOfCups) => {
-    const teaCups = [];
-
-    for (let cups = 1; cups <= numOfCups; cups += 1) {
-        const teaCup = prepareTea();
-        teaCups.push(teaCup);
-    }
-    return teaCups;
+// tabs is an array of titles of each site open within the window
+var Window = function (tabs) {
+    this.tabs = tabs; // We keep a record of the array inside the object
 };
 
-// Only change code below this line
-const tea4GreenTeamFCC = getTea(prepareGreenTea, 27);
-const tea4BlackTeamFCC = getTea(prepareBlackTea, 13);
-// Only change code above this line
+// When you join two windows into one window
+Window.prototype.join = function (otherWindow) {
+    this.tabs = this.tabs.concat(otherWindow.tabs);
+    return this;
+};
 
-console.log(tea4GreenTeamFCC, tea4BlackTeamFCC);
+// When you open a new tab at the end
+Window.prototype.tabOpen = function (tab) {
+    this.tabs.push("new tab"); // Let's open a new tab for now
+    return this;
+};
+
+// When you close a tab
+Window.prototype.tabClose = function (index) {
+    // Only change code below this line
+
+    this.tabs.splice(index, 1);
+
+    // Only change code above this line
+
+    return this;
+};
+
+// Let's create three browser windows
+var workWindow = new Window([
+    "GMail",
+    "Inbox",
+    "Work mail",
+    "Docs",
+    "freeCodeCamp",
+]); // Your mailbox, drive, and other work sites
+var socialWindow = new Window(["FB", "Gitter", "Reddit", "Twitter", "Medium"]); // Social sites
+var videoWindow = new Window(["Netflix", "YouTube", "Vimeo", "Vine"]); // Entertainment sites
+
+// Now perform the tab opening, closing, and other operations
+var finalTabs = socialWindow
+    .tabOpen() // Open a new tab for cat memes
+    .join(videoWindow.tabClose(2)) // Close third tab in video window, and join
+    .join(workWindow.tabClose(1).tabOpen());
+console.log(finalTabs.tabs);
