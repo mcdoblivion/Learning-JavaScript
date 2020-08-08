066fdaabecf9b56054fe2171cdc79f5829ee20ae
mcd_oblivion <mcd.oblivion@gmail.com>
2020-08-08 08:54:57 +0700

    Refactor Global Variables Out of Functions

--- FunctionalProgrammingChallenges.js
+++ FunctionalProgrammingChallenges.js
@@ -1,8 +1,40 @@
 // The global variable
-var fixedValue = 4;
+var bookList = [
+    "The Hound of the Baskervilles",
+    "On The Electrodynamics of Moving Bodies",
+    "Philosophiæ Naturalis Principia Mathematica",
+    "Disquisitiones Arithmeticae",
+];
 
-// Only change code below this line
-function incrementer(value) {
-    // Only change code above this line
-    return value + 1;
+// Change code below this line
+function add(bookList, bookName) {
+    let copyBookList = [];
+    for (let i = 0; i < bookList.length; i++) {
+        copyBookList.push(bookList[i]);
+    }
+    copyBookList.push(bookName);
+    return copyBookList;
+    // Change code above this line
 }
+
+// Change code below this line
+function remove(bookList, bookName) {
+    let copyBookList = [];
+    for (let i = 0; i < bookList.length; i++) {
+        copyBookList.push(bookList[i]);
+    }
+    let index = copyBookList.indexOf(bookName);
+    if (index >= 0) {
+        copyBookList.splice(index, 1);
+    }
+    return copyBookList;
+}
+
+var newBookList = add(bookList, "A Brief History of Time");
+var newerBookList = remove(bookList, "On The Electrodynamics of Moving Bodies");
+var newestBookList = remove(
+    add(bookList, "A Brief History of Time"),
+    "On The Electrodynamics of Moving Bodies"
+);
+
+console.log(bookList);
