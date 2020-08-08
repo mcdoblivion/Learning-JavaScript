a766ab9242346e121f83f7c8f011914bc43b40b3
mcd_oblivion <mcd.oblivion@gmail.com>
2020-08-08 08:12:39 +0700

    Pass Arguments-Avoid External Dependence in Function

--- FunctionalProgrammingChallenges.js
+++ FunctionalProgrammingChallenges.js
@@ -1,9 +1,8 @@
 // The global variable
 var fixedValue = 4;
 
-function incrementer () {
-  // Only change code below this line
-
-return fixedValue + 1;
-  // Only change code above this line
+// Only change code below this line
+function incrementer(value) {
+    // Only change code above this line
+    return value + 1;
 }
