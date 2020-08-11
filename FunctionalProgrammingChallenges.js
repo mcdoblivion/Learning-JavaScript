b06b43a64c22b453b468e973f8e523591699df97
mcd_oblivion <mcd.oblivion@gmail.com>
2020-08-11 08:27:03 +0700

    Add Elements to the End of an Array Using concat

--- FunctionalProgrammingChallenges.js
+++ FunctionalProgrammingChallenges.js
@@ -1,8 +1,9 @@
-function nonMutatingConcat(original, attach) {
+function nonMutatingPush(original, newItem) {
     // Only change code below this line
-    return original.concat(attach);
+    return original.concat(newItem);
+
     // Only change code above this line
 }
 var first = [1, 2, 3];
 var second = [4, 5];
-nonMutatingConcat(first, second);
+nonMutatingPush(first, second);
