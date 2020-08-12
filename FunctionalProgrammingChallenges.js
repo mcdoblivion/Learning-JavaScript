ebe1e1256ab95a561b26615d3ddf9c6c98971e20
mcd_oblivion <mcd.oblivion@gmail.com>
2020-08-12 10:26:22 +0700

    Check that Any Elements in Array Meet a Criteria

--- FunctionalProgrammingChallenges.js
+++ FunctionalProgrammingChallenges.js
@@ -1,6 +1,6 @@
 function checkPositive(arr) {
     // Only change code below this line
-    return arr.every((value) => value > 0);
+    return arr.some(value => value > 0);
     // Only change code above this line
 }
 checkPositive([1, 2, 3, -4, 5]);
