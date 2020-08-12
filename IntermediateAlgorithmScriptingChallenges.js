942e7f74337286e9521543b663b21130768c6003
mcd_oblivion <mcd.oblivion@gmail.com>
2020-08-12 15:29:30 +0700

    Diff Two Arrays

--- IntermediateAlgorithmScriptingChallenges.js
+++ IntermediateAlgorithmScriptingChallenges.js
@@ -1,10 +1,19 @@
-function sumAll(arr) {
-    arr.sort((a, b) => a - b);
-    let sum = 0;
-    for (let i = arr[0]; i <= arr[1]; i++) {
-        sum += i;
+function diffArray(arr1, arr2) {
+    let copy1 = arr1.slice();
+    let copy2 = arr2.slice();
+    for (let i = 0; i < copy1.length; i++) {
+        if (copy2.indexOf(copy1[i]) >= 0) {
+            copy2.splice(copy2.indexOf(copy1[i]), 1);
+            copy1.splice(i--, 1);
+        }
     }
-    return sum;
+    return copy1.concat(copy2);
 }
 
-sumAll([1, 4]);
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+console.log(
+    diffArray(
+        ["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"],
+        ["diorite", "andesite", "grass", "dirt", "dead shrub"]
+    )
+);
