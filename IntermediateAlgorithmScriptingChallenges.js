a08062392e001f2251cfbc4a09b094735146e6b0
mcd_oblivion <mcd.oblivion@gmail.com>
2020-08-15 10:01:30 +0700

    DNA Pairing

--- IntermediateAlgorithmScriptingChallenges.js
+++ IntermediateAlgorithmScriptingChallenges.js
@@ -1,10 +1,11 @@
-function myReplace(str, before, after) {
-    if (before[0] == before[0].toUpperCase()) {
-        after = after[0].toUpperCase() + after.substring(1);
-    } else {
-        after = after[0].toLowerCase() + after.substring(1);
-    }
-    return str.replace(before, after);
+function pairElement(str) {
+    let characters = str.split("");
+    return characters.map((char) => {
+        if (char == "A") return ["A", "T"];
+        else if (char == "T") return ["T", "A"];
+        else if (char == "C") return ["C", "G"];
+        else if (char == "G") return ["G", "C"];
+    });
 }
 
-myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
+pairElement("GCG");
