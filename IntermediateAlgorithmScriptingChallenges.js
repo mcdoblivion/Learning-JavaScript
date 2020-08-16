076f5341ca7e22751f29625baf603e6bbef00654
mcd_oblivion <mcd.oblivion@gmail.com>
2020-08-16 11:32:23 +0700

    Steamroller

--- IntermediateAlgorithmScriptingChallenges.js
+++ IntermediateAlgorithmScriptingChallenges.js
@@ -1,11 +1,17 @@
-function dropElements(arr, func) {
-    while (true) {
-        if (func(arr[0])) break;
-        else arr.shift();
+function steamrollArray(arr) {
+    let resultArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        resultArray.push(arr[i]);
     }
-    return arr;
+
+    let i = 0;
+    while (i < resultArray.length) {
+        while (Array.isArray(resultArray[i]))
+            resultArray.splice(i, 1, ...resultArray[i]);
+        i++;
+    }
+    return resultArray;
 }
 
-dropElements([1, 2, 3], function (n) {
-    return n < 3;
-});
+steamrollArray([1, [2], [3, [[4]]]]);
+console.log(steamrollArray([[["a"]], [["b"]]]));
