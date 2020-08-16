36a20d5d63e64ea835edc6a17e44e4ce5ec13c3d
mcd_oblivion <mcd.oblivion@gmail.com>
2020-08-16 11:44:50 +0700

    Binary Agents

--- IntermediateAlgorithmScriptingChallenges.js
+++ IntermediateAlgorithmScriptingChallenges.js
@@ -1,17 +1,13 @@
-function steamrollArray(arr) {
-    let resultArray = [];
-    for (let i = 0; i < arr.length; i++) {
-        resultArray.push(arr[i]);
-    }
-
-    let i = 0;
-    while (i < resultArray.length) {
-        while (Array.isArray(resultArray[i]))
-            resultArray.splice(i, 1, ...resultArray[i]);
-        i++;
-    }
-    return resultArray;
+function binaryAgent(str) {
+    let decimalCode = str.split(" ").map((binary) => parseInt(binary, 2));
+    return String.fromCharCode(...decimalCode);
 }
 
-steamrollArray([1, [2], [3, [[4]]]]);
-console.log(steamrollArray([[["a"]], [["b"]]]));
+binaryAgent(
+    "01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"
+);
+console.log(
+    binaryAgent(
+        "01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"
+    )
+);
