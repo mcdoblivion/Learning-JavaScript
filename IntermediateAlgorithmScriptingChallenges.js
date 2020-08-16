9b1cb0f47b32588082288e0c222c9ad15f4a07c6
mcd_oblivion <mcd.oblivion@gmail.com>
2020-08-16 08:53:40 +0700

    Sum All Odd Fibonacci NumbersPassed

--- IntermediateAlgorithmScriptingChallenges.js
+++ IntermediateAlgorithmScriptingChallenges.js
@@ -1,14 +1,16 @@
-function convertHTML(str) {
-    const htmlEntities = {
-        "&": "&amp;",
-        "<": "&lt;",
-        ">": "&gt;",
-        '"': "&quot;",
-        "'": "&apos;",
-    };
+function sumFibs(num) {
+    if (num == 1) return 2;
+    let fibonacci = [1, 1];
+    let sum = 2;
+    while (true) {
+        let nextNum =
+            fibonacci[fibonacci.length - 1] + fibonacci[fibonacci.length - 2];
+        if (nextNum > num) break;
 
-    return str.replace(/([&<>\"'])/g, match => htmlEntities[match]);
+        fibonacci.push(nextNum);
+        if (nextNum % 2 == 1) sum += nextNum;
+    }
+    return sum;
 }
 
-convertHTML("Dolce & Gabbana");
-console.log(convertHTML("Dolce & Gabbana"));
+sumFibs(4);
