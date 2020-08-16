390f58e5ed0c220277758bc5ad5a61c461028c64
mcd_oblivion <mcd.oblivion@gmail.com>
2020-08-16 11:52:57 +0700

    Everything Be True

--- IntermediateAlgorithmScriptingChallenges.js
+++ IntermediateAlgorithmScriptingChallenges.js
@@ -1,13 +1,18 @@
-function binaryAgent(str) {
-    let decimalCode = str.split(" ").map((binary) => parseInt(binary, 2));
-    return String.fromCharCode(...decimalCode);
+function truthCheck(collection, pre) {
+    for (let i = 0; i < collection.length; i++) {
+        if (collection[i].hasOwnProperty(pre)) {
+            if (!Boolean(collection[i][pre])) return false;
+        } else return false;
+    }
+    return true;
 }
 
-binaryAgent(
-    "01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"
-);
-console.log(
-    binaryAgent(
-        "01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"
-    )
+truthCheck(
+    [
+        { user: "Tinky-Winky", sex: "male" },
+        { user: "Dipsy", sex: "male" },
+        { user: "Laa-Laa", sex: "female" },
+        { user: "Po", sex: "female" },
+    ],
+    "sex"
 );
