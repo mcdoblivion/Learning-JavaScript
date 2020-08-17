3d25169a0edc923d284fe7413f92c7e5ca616f6b
mcd_oblivion <mcd.oblivion@gmail.com>
2020-08-17 16:05:33 +0700

    First challenge

--- PalindromeChecker.js
+++ PalindromeChecker.js
@@ -1,1 +1,9 @@
+function palindrome(str) {
+    // Remove character is not an word character and transform to lower case
+    let newStr = str.replace(/\W|_/g, "").toLowerCase();
+    // Reverse string an check if it change or not
+    return newStr === newStr.split("").reverse().join("");
+}
 
+palindrome("eye");
+console.log(palindrome("abbA"));
